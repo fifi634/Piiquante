d753d195ff981ffa9654d29d77b7a58edd4a157c
Philippe Cohen <cohen.philippe@hotmail.fr>
2022-08-05 16:20:08 +0200

    security(middleware/password.js): add password-validator plugin and configure it

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,14 @@
-// Express import
+// Import plugins
 const express = require('express');
+const password = require('../middleware/password');
+
 const router = express.Router();
 
 // Controller import
 const userCtrl = require('../controllers/user');
 
 // Path
-router.post('/signup', userCtrl.signup);
+router.post('/signup', password, userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 // Router export
